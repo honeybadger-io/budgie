80453dbc399d3daad1b3427b7a091b454d83d4a4
Benjamin Curtis <ben@bencurtis.com>
2024-04-12 06:06:52 -0700

    Add a rescue_from to FailingJob

--- app/jobs/failing_job.rb
+++ app/jobs/failing_job.rb
@@ -1,5 +1,11 @@
 class FailingJob < ApplicationJob
+  rescue_from StandardError do |exception|
+    Rails.logger.info "Rescuing"
+    true
+  end
+
   def perform(*args)
+    Rails.logger.info "FailingJob is running"
     raise "This job always fails"
   end
 end
